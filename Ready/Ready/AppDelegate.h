1d9c2676c50142c21585d527d9204e96add54aec
Mingyang <inkdot.l@gmail.com>
2015-01-17 13:48:44 -0800

    Setup

---
+++ Ready/Ready/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ready
+//
+//  Created by Lee on 1/17/15.
+//  Copyright (c) 2015 LetUsRock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
